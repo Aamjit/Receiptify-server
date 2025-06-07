9aa79335f57fb0eea632c0eede4f52050a017c39
Amarjti Yanglem <aayanglem@gmail.com>
2025-06-07 13:12:49 +0530

    update: generate PDF on server using puppeteer

--- server.js
+++ server.js
@@ -4,25 +4,43 @@ const sgMail = require('@sendgrid/mail')
 const puppeteer = require('puppeteer');
 const app = express();
 const os = require('os');
+const fs = require('fs');
+const generateHTMLForReport = require('./utils/generateHTMLForReport');
 
 app.use(express.json());
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-app.post('/send-email', async (req, res) => {
+
+// **********   MIDDLEWARE FUNCTION **********
+const checkAppId = (req, res, next) => {
     const appId = req.header('X-App-Id');
     if (appId !== process.env.ALLOWED_APP_ID) {
         return res.status(403).json({ error: 'Forbidden: Invalid App Id' });
     }
+    next();
+}
 
+
+// **********   SEND MAIL   **********
+app.post('/send-email', checkAppId, async (req, res) => {
     const { to, subject, text, html, name } = req?.body;
+    const screenshotPath = `temp/debug_${Date.now()}_${Math.random().toString(36).slice(2)}.png`;
 
     let pdfBuffer;
     try {
         const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         const page = await browser.newPage();
-        await page.setContent(html || '', { waitUntil: 'networkidle0' });
-        pdfBuffer = await page.pdf({ format: 'A4' });
+        // await page.setViewport({ width: 1280, height: 900 });
+        await page.setContent(html, { waitUntil: 'networkidle0' });
+        await page.evaluateHandle('document.fonts.ready');
+        await page.evaluate(() => {
+            return Promise.all(Array.from(document.images).filter(i => !i.complete).map(i => new Promise(resolve => { i.onload = i.onerror = resolve; })));
+        });
+
+        await page.screenshot({ path: screenshotPath, fullPage: true });
+
+        pdfBuffer = await page.pdf({ format: 'letter' });
         await browser.close();
     } catch (err) {
         console.error('PDF generation error:', err);
@@ -30,18 +48,21 @@ app.post('/send-email', async (req, res) => {
     }
 
     const msg = {
-        to: [...to], // Change to your recipient
-        from: 'aayanglem@gmail.com', // Change to your verified sender
+        to: [...to],
+        from: 'aayanglem@gmail.com',
         subject: subject,
         text: text,
-        attachments: [
-            {
-                content: Buffer.from(pdfBuffer).toString('base64').trim(),
-                filename: `SalesReport_${name}.pdf`,
-                type: 'application/pdf',
-                disposition: 'attachment',
-            },
-        ],
+        attachments: [{
+            content: Buffer.from(pdfBuffer).toString('base64').trim(),
+            filename: `SalesReport_${name}.pdf`,
+            type: 'application/pdf',
+            disposition: 'attachment',
+        }, {
+            content: Buffer.from(fs.readFileSync(screenshotPath)).toString('base64').trim(),
+            filename: `SalesReport_${name}_screenshot.png`,
+            type: 'image/png',
+            disposition: 'attachment',
+        }],
     }
 
     sgMail
@@ -60,10 +81,29 @@ app.post('/send-email', async (req, res) => {
             }
         })
         .finally(() => {
+            fs.unlinkSync(screenshotPath);
             return;
         })
 });
 
+
+// ********** GENERATE EMAIL **********
+app.post('/generate-email', checkAppId, async (req, res) => {
+    const { averageTransaction, businessInfo, dailySales, dailySalesCount, selectedRangeLabel, topItems, totalSales } = req?.body;
+
+    const html = generateHTMLForReport({ averageTransaction, businessInfo, dailySales, dailySalesCount, selectedRangeLabel, topItems, totalSales })
+
+    if (!html.length > 0) {
+        return res.status(500).json({ error: 'Failed: Template generation' });
+    }
+
+    return res.status(200).send({
+        html: html
+    })
+
+});
+
+// **********   STARTING SERVER     **********
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     const networkInterfaces = os.networkInterfaces();
